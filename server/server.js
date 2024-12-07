c0fb36e10b88baefc8c55686cf92d19c0b3c3377
an12iket <aniketchavan121012@gmail.com>
2024-12-07 10:43:37 +0530

    added DB schema, environment variables

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -6,7 +7,14 @@ const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
+const connectDB = require("./config/dbConn")
+const mongoose = require("mongoose")
+const { logEvents } = require('./middleware/logger')
 const PORT = process.env.PORT || 3500
+
+console.log(process.env.NODE_ENV);
+
+connectDB();
 
 app.use(logger)
 
@@ -25,7 +33,7 @@ app.all('*', (req, res) => {
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
     } else if (req.accepts('json')) {
-        res.json({Message : "404 Not found"})
+        res.json({ Message: "404 Not found" })
     } else {
         res.type('txt').send('404 Not found')
     }
@@ -33,4 +41,12 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler)
 
-app.listen(PORT, () => {console.log(`Server running on PORT ${PORT}`);})
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => { console.log(`Server running on PORT ${PORT}`); })
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err);
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
