7c5178fe076420d69e78142074713d7b609da27c
an12iket <aniketchavan121012@gmail.com>
2024-12-07 16:13:49 +0530

    minor change

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 
 const getAllUsers = asyncHandler(async (req, res) => {
     const users = await User.find().select('-password').lean();
-    if(!users){
+    if(!users?.length){
         return res.status(400).json({
             message: "No users found"
         })
