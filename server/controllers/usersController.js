a39eabcc750c7bb04ce3523247234baa618908bd
an12iket <aniketchavan121012@gmail.com>
2024-12-07 15:55:28 +0530

    created updateUsers controller

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -12,6 +12,7 @@ const getAllUsers = asyncHandler(async (req, res) => {
     }
     res.json(users);
 })
+// create user
 const createNewUsers = asyncHandler(async (req, res) => {
     const { username, password, roles } = req.body;
     if(!username || !password || !Array.isArray(roles) || !roles.length){
@@ -44,8 +45,48 @@ const createNewUsers = asyncHandler(async (req, res) => {
         })
     }
 })
+// Update user
 const updateUsers = asyncHandler(async (req, res) => {
+    const { id, username, roles, active, password } = req.body;
 
+    // confirm data
+    if(!id || !username || !Array.isArray(roles) || !roles.length || typeof active!== 'boolean'){
+        return res.status(400).json({
+            message: "All fields are required!"
+        })
+    }
+
+    const user = await User.findById(id).exec();
+
+    if(!user){
+        return res.status(400).json({
+            message: "User not found"
+        })
+    }
+
+    // Duplicate
+    const duplicate = await User.findOne({ username }).lean().exec();
+    // Allow updates to the original user
+    if(duplicate && duplicate?._id.toString() !== id){
+        return res.status(409).json({
+            message: "duplicate username"
+        })
+    }
+
+    user.username = username
+    user.roles = roles
+    user.active = active
+
+    if(password){
+        // Hash password
+        user.password = await bcrypt.hash(password, 10) // salt rounds
+    }
+
+    const updatedUsers = await user.save()
+
+    res.json({
+        message: `${updateUsers.username} updated`
+    })
 })
 const deleteUsers = asyncHandler(async (req, res) => {
 
